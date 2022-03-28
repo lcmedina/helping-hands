ea4cd6a4546d2ede4462ca5177766cc6071d2c70
Laura Medina <lcmedina390@gmail.com>
2022-03-28 16:12:02 -0500

    home page and menu styling

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import stroller from './images/stroller.png'
 
 function App() {
   return (
+    <>
     <div className='app'>
     <TitleBar />
     <p className='heading'>Helping Hands</p>
     <button className='call-to-action'>Login</button>
-    <button>Sign Up</button>
+    <button className='button'>Sign Up</button>
     <img src={stroller} width={255} height={215} />
     </div>
+    </>
   );
 }
 
