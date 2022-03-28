863da05f9999ddc3acb809480337109207c930cb
Laura Medina <lcmedina390@gmail.com>
2022-03-27 21:19:50 -0500

    home page

---
+++ src/components/TitleBar.js
@@ -0,0 +1,21 @@
+import { Nav, Navbar, Container } from 'react-bootstrap';
+
+const TitleBar = () => {
+    return ( 
+        <Navbar expand="lg" collapseOnSelect>
+  <Container>
+    <Navbar.Brand href="#home">Helping Hands</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Donate</Nav.Link>
+        <Nav.Link href="#link">Join</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+    </Navbar>
+     );
+}
+ 
+export default TitleBar;
